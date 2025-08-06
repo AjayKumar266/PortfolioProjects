6d005fdfe821c2578826b8427739eb84a4616290
Ajay Kumar <ajaykumar51198@gmail.com>
2025-08-06 16:31:48 +0530

    Update Portfolio Project - Data Cleaning.sql

--- Portfolio Project - Data Cleaning.sql
+++ Portfolio Project - Data Cleaning.sql
@@ -18,9 +18,9 @@ SELECT * FROM world_layoffs.layoffs;
 
 
 -- follow these steps to clean data
--- 1. check for duplicates and remove any
--- 2. standardize data and fix errors
--- 3. Look at null values and see what 
+-- 1. Look for duplicates and remove them
+-- 2. standardize data
+-- 3. Look at null values
 -- 4. remove any columns and rows that are not necessary
 
 
@@ -60,7 +60,6 @@ SELECT *
 FROM world_layoffs.layoffs_staging
 WHERE company = 'Oda'
 ;
--- it looks like these are all legitimate entries and shouldn't be deleted. We need to really look at every single row to be accurate
 
 -- these are our real duplicates 
 SELECT *
@@ -74,8 +73,6 @@ FROM (
 ) duplicates
 WHERE 
 	row_num > 1;
-
--- these are the ones we want to delete where the row number is > 1 or 2or greater essentially
 
 -- now you may want to write it like this:
 WITH DELETE_CTE AS 
